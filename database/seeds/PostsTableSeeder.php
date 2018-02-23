9f8fbe5441b2c11ff52ca8c7df88e1a91897b548
Unknown <ramon.mayor.lopez@gmail.com>
2018-02-23 17:44:18 +0100

    clase 48

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -24,6 +24,7 @@ class PostsTableSeeder extends Seeder
         $post->body='<p>Este es mi primer posts</p>';
         $post->published_at=Carbon::now()->subDay(4);
         $post->category_id=1;
+        $post->user_id=1;
         $post->save();
 
         $post->tags()->attach(['1', '2']);
@@ -36,6 +37,7 @@ class PostsTableSeeder extends Seeder
         $post->body='<p>Este es mi segundo posts</p>';
         $post->published_at=Carbon::now()->subDay(3);
         $post->category_id=2;
+        $post->user_id=1;
         $post->save();
 
         $post->tags()->attach(['2', '3']);
@@ -48,6 +50,7 @@ class PostsTableSeeder extends Seeder
         $post->body='<p>Este es mi tercer posts</p>';
         $post->published_at=Carbon::now()->subDay(2);
         $post->category_id=1;
+        $post->user_id=1;
         $post->save();
 
         $post->tags()->attach(['3', '4']);
@@ -59,6 +62,7 @@ class PostsTableSeeder extends Seeder
         $post->body='<p>Este es mi cuarto posts</p>';
         $post->published_at=Carbon::now()->subDay(1);
         $post->category_id=2;
+        $post->user_id=1;
         $post->save();
 
         $post->tags()->attach(['4', '1']);
